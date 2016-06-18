70c2ed2129b8d29dee8a2cd06e53d4cc99fc3408
Matt Latham <latham75@gmail.com>
2016-06-18 16:57:26 -0500

    info array finished

--- private.js
+++ private.js
@@ -1,2 +1,35 @@
-// iife to store all private donor info in array of objects
-// {name: "" pledge: ""}
+
+var Donations = (function (){
+	var info = [
+		{name: "Jack",
+		pledge: 50,
+		frequency: "single"},
+		
+		{name: "Jill",
+		pledge: 75,
+		frequency: "single"},
+		
+		{name: "John",
+		pledge: 20,
+		frequency: "single"},
+		
+		{name: "Jane",
+		pledge: 5,
+		frequency: "per lap"},
+		
+		{name: "Jimmy Crackcorn",
+		pledge: 0.10,
+		frequency: "per lap"}
+	];
+
+	return{
+		getInfo: function(){
+			return info;
+		},
+		setInfo: function (newData){
+			info.push(newData);
+		}
+	};
+
+
+})(Donations);
