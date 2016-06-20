472ddef80044b06a0d082a21d6c9345f0db0de5c
Matt Latham <latham75@gmail.com>
2016-06-19 19:25:08 -0500

    fixed handlers, gets object data

--- domhandler.js
+++ domhandler.js
@@ -3,25 +3,38 @@
 // same returns info to table
 // 
 var userName = document.getElementById("custname");
-var userEmail = document.getElementById("custEmail");
+var userEmail = document.getElementById("custemail");
 var radios = document.getElementsByName("donation");
 var amount = document.getElementById("money");
 var button = document.getElementById("donbut");
 var cancel = document.getElementById("canbut");	
 
-var newInfo = [];
-button.addEventListener("click", function tally(){
-	var newDonor = {};
-	newDonor.name = userName.value;
-	newDonor.pledge = amount.value;
-	for (var i = 0; i < radios.length; i++) {
-    if (radios[i].checked) {
-    	newDonor.frequency = radios.value;
-        break;
-    };
-    
-};
 
+var newDonor = {};
+button.addEventListener("click", function tally(){
+	console.log("event", event);
+	newDonor.name = userName.value;
+	newDonor.email = userEmail.value;
+	newDonor.pledge = amount.value;
+	// for (var i = 0; i < radios.length; i++) {
+ //    if (radios[i].checked) {
+ //    	console.log("radio", radios[i].checked)
+ //    	newDonor.frequency = radios[i].checked.value;
+ //        break;
+ //    };
+    if (document.getElementById('r1').checked) {
+  	newDonor.frequency = document.getElementById("r1").value;
+	} else {newDonor.frequency = document.getElementById("r2").value;}
+    // info.push(newDonor);
+;
+console.log("new", newDonor);
+
+});
+
+cancel.addEventListener("click", function clear(){
+	userName.value = "";
+	userEmail.value = "";
+	amount.value = "";
 })
 
 
--- private.js
+++ private.js
@@ -2,22 +2,27 @@
 var Donations = (function (oldDonations){
 	var info = [
 		{name: "Jack",
+		email: "blah@gmail.com",
 		pledge: 50,
 		frequency: "single"},
 		
 		{name: "Jill",
+		email: "blah@gmail.com",
 		pledge: 75,
 		frequency: "single"},
 		
 		{name: "John",
+		email: "blah@gmail.com",
 		pledge: 20,
 		frequency: "single"},
 		
 		{name: "Jane",
+		email: "blah@gmail.com",
 		pledge: 5,
 		frequency: "per lap"},
 		
 		{name: "Jimmy Crackcorn",
+		email: "eyup@gmail.com",
 		pledge: 0.10,
 		frequency: "per lap"}
 	];
